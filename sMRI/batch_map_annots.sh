e953b5b9a7febf535771d06553b473a50942ea01
kate mills <klmills@uoregon.edu>
2017-04-28 12:36:19 -0700

    added scripts and templates and lists

---
+++ sMRI/batch_map_annots.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# This batch file calls on your subject
+# list (named subject_list.txt). And 
+# runs the job_SUMA.sh file for 
+# each subject. It saves the ouput
+# and error files in their specified
+# directories.
+#
+# Set your study
+STUDY=/projects/dsnlab/tag
+
+# Set subject list
+SUBJLIST=`cat subject_list.txt`
+
+# 
+for SUBJ in $SUBJLIST
+do
+sbatch --export SUBID=${SUBJ} --job-name mapANNOTs --partition=defq --mem-per-cpu=2G --cpus-per-task=1 -o "${STUDY}"/sMRI/scripts/output/"${SUBJ}"_mapANNOTs_output.txt -e "${STUDY}"/sMRI/scripts/output/"${SUBJ}"_mapANNOTs_error.txt job_map_annots.sh
+done
