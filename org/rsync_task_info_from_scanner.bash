b13ab778d8e4580c8748a9c5c85df9c27d1f58b2
John Flournoy <uoflournoy@gmail.com>
2018-03-16 16:43:29 -0400

    updated to also copy data from mock scan computer

--- org/rsync_task_info_from_scanner.bash
+++ org/rsync_task_info_from_scanner.bash
@@ -5,6 +5,13 @@ then
     echo "usage: bash `basename $0` /directory/to/receive/task/info/"
     echo "Note that there should be a trailing '/'"
 else
+    SIMDIR="$1/simulatum_data/"
     echo "Connecting to scan-room computer..."
     rsync -aiv -e ssh dsnlab@kashkaval:/Users/dsnlab/Studies/TAG/DRS/task/input/tag*_info.mat $1
+    if [ ! -d "$SIMDIR"]; then
+        mkdir -p $SIMDIR
+    fi
+    echo "Will save mock-scan data to directory: $SIMDIR"
+    echo "Connecting to mock-scan computer..."
+    rsync -aiv -e ssh dsnlab@simulatum:/Users/dsnlab/Studies/TAG/task/DRS/task/input/tag*_info.mat $SIMDIR
 fi
