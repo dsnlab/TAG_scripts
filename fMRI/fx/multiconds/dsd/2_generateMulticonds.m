835d9e7ff663bae560f53413974e2d361921df02
Nandita Vijayakumar <nanditavijayakumar@Nandita-Vijayakumars-MacBook-Pro-2.local>
2018-06-06 09:24:54 -0700

    updating DSD scripts

--- fMRI/fx/multiconds/dsd/2_generateMulticonds.m
+++ fMRI/fx/multiconds/dsd/2_generateMulticonds.m
@@ -9,8 +9,8 @@
 clear all
 
 %setting directory and listing csv files in matlab directory
-f = '/Volumes/psych-cog/dsnlab/TAG/TAG_BIDS/derivatives/fMRI/fx/multiconds/DSD/ses-wave1/Summary/';
-g = '/Volumes/psych-cog/dsnlab/TAG/TAG_BIDS/derivatives/fMRI/fx/multiconds/DSD/ses-wave1/';
+f = '/Volumes/psych-cog/dsnlab/TAG/nonbids_data/fmri/fx/multiconds/dsd/wave1/Summary/';
+g = '/Volumes/psych-cog/dsnlab/TAG/nonbids_data/fmri/fx/multiconds/dsd/wave1/NOD/';
 d = dir(fullfile(f,'*summary.csv'));
 
 for k=1:length(d)
@@ -19,14 +19,15 @@ cd (f)
 
 filename = d(k).name;
 fid=fopen(filename, 'r');
-M = textscan(fid,'%s%s%s%s\n','delimiter',',','Headerlines',1);
+M = textscan(fid,'%s%s%s%f%f%s\n','delimiter',',','Headerlines',1); 
 fclose(fid);
 
 %changing durations from double to cell format
+M{5} = num2cell(M{5});
 M{4} = num2cell(M{4});
 
 %creating a dataframe of names, onsets, durations
-Mdata = [M{2} M{3} M{4}];
+Mdata = [M{3} M{4} M{5} M{6}];
 
 %create separate data arrays for each event
 for i=1:length(Mdata) % for each row in Mdata
@@ -34,85 +35,37 @@ for i=1:length(Mdata) % for each row in Mdata
         E1{i,1} = Mdata{i,1};
         E1{i,2} = Mdata{i,2};
         E1{i,3} = Mdata{i,3};
+        E1{i,4} = Mdata{i,4};
     else if Mdata{i,1}==string('2') %ditto for event 2 
         E2{i,1} = Mdata{i,1};
         E2{i,2} = Mdata{i,2};
         E2{i,3} = Mdata{i,3};
+        E2{i,4} = Mdata{i,4};
     else if Mdata{i,1}==string('3')%event 3 
         E3{i,1} = Mdata{i,1};
         E3{i,2} = Mdata{i,2};
         E3{i,3} = Mdata{i,3};
+        E3{i,4} = Mdata{i,4};
     else if Mdata{i,1}==string('4')%event 4 
         E4{i,1} = Mdata{i,1};
         E4{i,2} = Mdata{i,2};
         E4{i,3} = Mdata{i,3};
+        E4{i,4} = Mdata{i,4};
     else if Mdata{i,1}==string('5')%event 5
         E5{i,1} = Mdata{i,1};
         E5{i,2} = Mdata{i,2};
         E5{i,3} = Mdata{i,3};
-    else if Mdata{i,1}==string('6')%event 5
+        E5{i,4} = Mdata{i,4};
+    else if Mdata{i,1}==string('6')%event 6
         E6{i,1} = Mdata{i,1};
         E6{i,2} = Mdata{i,2};
         E6{i,3} = Mdata{i,3};
-    else if Mdata{i,1}==string('7')%event 5
+        E6{i,4} = Mdata{i,4};
+    else if Mdata{i,1}==string('7')%event 7
         E7{i,1} = Mdata{i,1};
         E7{i,2} = Mdata{i,2};
         E7{i,3} = Mdata{i,3};
-    else if Mdata{i,1}==string('8')%event 5
-        E8{i,1} = Mdata{i,1};
-        E8{i,2} = Mdata{i,2};
-        E8{i,3} = Mdata{i,3};
-    else if Mdata{i,1}==string('9')%event 5
-        E9{i,1} = Mdata{i,1};
-        E9{i,2} = Mdata{i,2};        
-        E9{i,3} = Mdata{i,3};
-    else if Mdata{i,1}==string('10')%event 5
-        E10{i,1} = Mdata{i,1};
-        E10{i,2} = Mdata{i,2};
-        E10{i,3} = Mdata{i,3};
-    else if Mdata{i,1}==string('11')%event 5
-        E11{i,1} = Mdata{i,1};
-        E11{i,2} = Mdata{i,2};
-        E11{i,3} = Mdata{i,3};
-    else if Mdata{i,1}==string('12')%event 5
-        E12{i,1} = Mdata{i,1};
-        E12{i,2} = Mdata{i,2};
-        E12{i,3} = Mdata{i,3};
-    else if Mdata{i,1}==string('13')%event 5
-        E13{i,1} = Mdata{i,1};
-        E13{i,2} = Mdata{i,2};
-        E13{i,3} = Mdata{i,3};
-    else if Mdata{i,1}==string('14')%event 5
-        E14{i,1} = Mdata{i,1};
-        E14{i,2} = Mdata{i,2};
-        E14{i,3} = Mdata{i,3};
-    else if Mdata{i,1}==string('15')%event 5
-        E15{i,1} = Mdata{i,1};
-        E15{i,2} = Mdata{i,2};
-        E15{i,3} = Mdata{i,3};
-    else if Mdata{i,1}==string('16')%event 5
-        E16{i,1} = Mdata{i,1};
-        E16{i,2} = Mdata{i,2};
-        E16{i,3} = Mdata{i,3};
-    else if Mdata{i,1}==string('17')%event 5
-        E17{i,1} = Mdata{i,1};
-        E17{i,2} = Mdata{i,2};
-        E17{i,3} = Mdata{i,3};        
-    else if Mdata{i,1}==string('18')%event 5
-        E18{i,1} = Mdata{i,1};
-        E18{i,2} = Mdata{i,2};
-        E18{i,3} = Mdata{i,3};   
-        end
-        end
-        end
-        end
-        end
-        end
-        end
-        end
-        end
-        end
-        end
+        E7{i,4} = Mdata{i,4};
         end
         end
         end
@@ -120,9 +73,9 @@ for i=1:length(Mdata) % for each row in Mdata
         end
         end
     end
-end
+end     
 
-for j=1:18
+for j=1:7
     if exist(strcat('E',num2str(j))) == 1
         n = eval(strcat('E',num2str(j)));
         n(all(cellfun('isempty',n),2),:)=[];
@@ -130,56 +83,93 @@ for j=1:18
     end
 end
 
+E1(all(cellfun('isempty',E1),2),:)=[];
+E2(all(cellfun('isempty',E2),2),:)=[];
+E3(all(cellfun('isempty',E3),2),:)=[];
+E4(all(cellfun('isempty',E4),2),:)=[];
+E5(all(cellfun('isempty',E5),2),:)=[];
+E6(all(cellfun('isempty',E6),2),:)=[];
+E7(all(cellfun('isempty',E7),2),:)=[];
 
 %create array of names
-names={'affect_true','affect_false','neutral_true','neutral_false',...
-       'affect_share_loss','affect_share_gain','affect_share_none',...
-       'affect_private_loss','affect_private_gain','affect_private_none',...
-       'neutral_share_loss','neutral_share_gain','neutral_share_none',...
-       'neutral_private_loss','neutral_private_gain','neutral_private_none',...
-       'statement_missing','disclosure_missing'}; %generate names
+names={'neutral_statement','affect_statement','affect_share',...
+       'affect_private','neutral_share','neutral_private','disc_missing'}; %generate names
 
 %hacky way of creating arrays for onsets and durations that are same size
 %as names, but replace values with []
 
 onsets=names;
 
-for i=1:18
+for i=1:7
   a=onsets{i};
-    a=[]    
+    a=[]; 
     onsets{i}=a;
 end
 
 %input values from Mdata into onsets array
 
-for j=1:length(E)
+for j=1:7
     if isempty(E{1,j})
         onsets{1,j}=[];
     else onsets{1,j}=E{1,j}(:,2);
-         onsets{1,j}=str2double(onsets{1,j});
-         
     end
 end
 
-%input values from Mdata into onsets array
+onsets = cellfun(@cell2mat, onsets, 'UniformOutput', false)
+
+
+%input values from Mdata into durations array
 
 durations=onsets;
 
-for i=1:18
-    if isempty(durations{i})
-        durations{1,i}=[];
-    else durations{1,i}=E{1,i}(:,3);
-         durations{1,i}=cellfun(@str2double, durations{1,i});
+for j=1:7
+    if isempty(E{1,j})
+        durations{1,j}=[];
+    else durations{1,j}=E{1,j}(:,3);
     end
 end
 
+durations = cellfun(@cell2mat, durations, 'UniformOutput', false)
 
+
+%% PMODS
+x3 = E{1,3}(:,4);
+x3 = cellstr(x3);
+x3 = str2double(x3);
+
+x4 = E{1,4}(:,4);
+x4 = cellstr(x4);
+x4 = str2double(x4);
+
+x5 = E{1,5}(:,4);
+x5 = cellstr(x5);
+x5 = str2double(x5);
+
+x6 = E{1,6}(:,4);
+x6 = cellstr(x6);
+x6 = str2double(x6);
+
+pmod = struct('name',{''},'param', {}, 'poly', {});
+pmod(3).name{1} = 'NeuPr_pmod';
+pmod(3).param{1}=x3;
+pmod(3).poly{1}=1;
+
+pmod(4).name{1} = 'NeuSh_pmod';
+pmod(4).param{1}=x4;
+pmod(4).poly{1}=1;
+
+pmod(5).name{1} = 'AffPr_pmod';
+pmod(5).param{1}=x5;
+pmod(5).poly{1}=1;
+
+pmod(6).name{1} = 'AffSh_pmod';
+pmod(6).param{1}=x6;
+pmod(6).poly{1}=1;
+ 
 %% SAVE
-%sub ID
 saveName=strcat(M{1}(1),'_NOD.mat') % 
-cd (g) %need to change this to the output folder
-%save(saveName{1},'names','onsets','durations')
-save(saveName{1},'names','onsets','durations')
+cd (g) 
+save(saveName{1},'names','onsets','durations','pmod')
 
 clearvars -except f g d
 
