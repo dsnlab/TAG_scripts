99e682250d6414ea07b405f52f06be8a2c18cf4e
Nandita Vijayakumar <nanditavijayakumar@Nandita-Vijayakumars-MacBook-Pro-2.local>
2018-06-25 14:03:44 -0700

    updating DSD models

---
+++ fMRI/fx/models/dsd/wave1/contrasts/cond2.m
@@ -0,0 +1,37 @@
+%-----------------------------------------------------------------------
+% Job saved on 17-Jun-2018 20:22:20 by cfg_util (rev $Rev: 6460 $)
+% spm SPM - SPM12 (6906)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.stats.con.spmmat = {'/projects/dsnlab/shared/tag/nonbids_data/fMRI/fx/models/dsd/wave1/fd0.8_dv2.0_Neighbors/sub-TAG001/SPM.mat'};
+matlabbatch{1}.spm.stats.con.consess{1}.tcon.name = 'affect_state';
+matlabbatch{1}.spm.stats.con.consess{1}.tcon.weights = [1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0];
+matlabbatch{1}.spm.stats.con.consess{1}.tcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.consess{2}.tcon.name = 'neutral_state';
+matlabbatch{1}.spm.stats.con.consess{2}.tcon.weights = [0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0];
+matlabbatch{1}.spm.stats.con.consess{2}.tcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.consess{3}.tcon.name = 'affect_sh';
+matlabbatch{1}.spm.stats.con.consess{3}.tcon.weights = [0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0];
+matlabbatch{1}.spm.stats.con.consess{3}.tcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.consess{4}.tcon.name = 'neutral_sh';
+matlabbatch{1}.spm.stats.con.consess{4}.tcon.weights = [0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0];
+matlabbatch{1}.spm.stats.con.consess{4}.tcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.consess{5}.tcon.name = 'affect_pri';
+matlabbatch{1}.spm.stats.con.consess{5}.tcon.weights = [0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0];
+matlabbatch{1}.spm.stats.con.consess{5}.tcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.consess{6}.tcon.name = 'neutral_pri';
+matlabbatch{1}.spm.stats.con.consess{6}.tcon.weights = [0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0];
+matlabbatch{1}.spm.stats.con.consess{6}.tcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.consess{7}.tcon.name = 'affect_sh_pmod';
+matlabbatch{1}.spm.stats.con.consess{7}.tcon.weights = [0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0];
+matlabbatch{1}.spm.stats.con.consess{7}.tcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.consess{8}.tcon.name = 'neutral_sh_pmod';
+matlabbatch{1}.spm.stats.con.consess{8}.tcon.weights = [0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0];
+matlabbatch{1}.spm.stats.con.consess{8}.tcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.consess{9}.tcon.name = 'affect_pri_pmod';
+matlabbatch{1}.spm.stats.con.consess{9}.tcon.weights = [0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0];
+matlabbatch{1}.spm.stats.con.consess{9}.tcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.consess{10}.tcon.name = 'neutral_pri_pmod';
+matlabbatch{1}.spm.stats.con.consess{10}.tcon.weights = [0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0];
+matlabbatch{1}.spm.stats.con.consess{10}.tcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.delete = 0;
