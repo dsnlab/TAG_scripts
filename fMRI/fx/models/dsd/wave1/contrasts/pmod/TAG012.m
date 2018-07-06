5f886c40dd2d0931b59b1c2db4a2859ff0b3d095
Nandita Vijayakumar <nanditavijayakumar@Nandita-Vijayakumars-MacBook-Pro-2.local>
2018-07-06 08:54:58 -0700

    creating decision pmod models

---
+++ fMRI/fx/models/dsd/wave1/contrasts/pmod/TAG012.m
@@ -0,0 +1,25 @@
+%-----------------------------------------------------------------------
+% Job saved on 01-Jul-2018 20:22:20 by cfg_util (rev $Rev: 6460 $)
+% spm SPM - SPM12 (6906)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.stats.con.spmmat = {'/Volumes/psych-cog/dsnlab/TAG/nonbids_data/fMRI/fx/models/dsd/wave1/pmod/sub-TAG012/SPM.mat'};
+matlabbatch{1}.spm.stats.con.consess{1}.tcon.name = 'affect_state';
+matlabbatch{1}.spm.stats.con.consess{1}.tcon.weights = [1 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 ];
+matlabbatch{1}.spm.stats.con.consess{1}.tcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.consess{2}.tcon.name = 'neutral_state';
+matlabbatch{1}.spm.stats.con.consess{2}.tcon.weights = [0 1 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 ];
+matlabbatch{1}.spm.stats.con.consess{2}.tcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.consess{3}.tcon.name = 'affect_decision';
+matlabbatch{1}.spm.stats.con.consess{3}.tcon.weights = [0 0 1 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 ];
+matlabbatch{1}.spm.stats.con.consess{3}.tcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.consess{4}.tcon.name = 'neutral_decision';
+matlabbatch{1}.spm.stats.con.consess{4}.tcon.weights = [0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 ];
+matlabbatch{1}.spm.stats.con.consess{4}.tcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.consess{5}.tcon.name = 'affect_value_pmod';
+matlabbatch{1}.spm.stats.con.consess{5}.tcon.weights = [0 0 0 1 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 ];
+matlabbatch{1}.spm.stats.con.consess{5}.tcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.consess{6}.tcon.name = 'neutral_value_pmod';
+matlabbatch{1}.spm.stats.con.consess{6}.tcon.weights = [0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 ];
+matlabbatch{1}.spm.stats.con.consess{6}.tcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.delete = 0;
