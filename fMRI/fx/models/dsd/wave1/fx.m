cd2691a03f44322da63847d85da59c2027807f51
Nandi Vijayakumar <nanditav@talapas-ln1.cm.cluster>
2018-07-06 15:52:22 -0700

    cleaning up dsd fx scripts

--- fMRI/fx/models/dsd/wave1/fx.m
+++ fMRI/fx/models/dsd/wave1/fx.m
@@ -1,9 +1,9 @@
 %-----------------------------------------------------------------------
-% Job saved on 17-Jun-2018 10:47:31 by cfg_util (rev $Rev: 6460 $)
+% Job saved on 01-Jul-2018 22:03:53 by cfg_util (rev $Rev: 6460 $)
 % spm SPM - SPM12 (6906)
 % cfg_basicio BasicIO - Unknown
 %-----------------------------------------------------------------------
-matlabbatch{1}.cfg_basicio.file_dir.dir_ops.cfg_mkdir.parent = {'/projects/dsnlab/shared/tag/nonbids_data/fMRI/fx/models/dsd/wave1'};
+matlabbatch{1}.cfg_basicio.file_dir.dir_ops.cfg_mkdir.parent = {'/projects/dsnlab/shared/tag/nonbids_data/fMRI/fx/models/dsd/wave1/pmod'};
 matlabbatch{1}.cfg_basicio.file_dir.dir_ops.cfg_mkdir.name = 'sub-TAG001';
 matlabbatch{2}.cfg_basicio.file_dir.file_ops.file_fplist.dir = {'/projects/dsnlab/shared/tag/bids_data/derivatives/fmriprep/sub-TAG001/ses-wave1/func/'};
 matlabbatch{2}.cfg_basicio.file_dir.file_ops.file_fplist.filter = '^sub-TAG001_ses-wave1_task-DSD_run-01_bold_space-MNI152NLin2009cAsym_preproc.nii.gz';
@@ -38,13 +38,13 @@ matlabbatch{10}.spm.stats.fmri_spec.timing.fmri_t = 72;
 matlabbatch{10}.spm.stats.fmri_spec.timing.fmri_t0 = 36;
 matlabbatch{10}.spm.stats.fmri_spec.sess(1).scans(1) = cfg_dep('Smooth: Smoothed Images', substruct('.','val', '{}',{8}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','files'));
 matlabbatch{10}.spm.stats.fmri_spec.sess(1).cond = struct('name', {}, 'onset', {}, 'duration', {}, 'tmod', {}, 'pmod', {}, 'orth', {});
-matlabbatch{10}.spm.stats.fmri_spec.sess(1).multi = {'/projects/dsnlab/shared/tag/nonbids_data/fMRI/fx/multiconds/dsd/wave1/NOD/001_DSD1_NOD.mat'};
+matlabbatch{10}.spm.stats.fmri_spec.sess(1).multi = {'/projects/dsnlab/shared/tag/nonbids_data/fMRI/fx/multiconds/dsd/wave1/NOD/pmod/001_DSD1_NOD.mat'};
 matlabbatch{10}.spm.stats.fmri_spec.sess(1).regress = struct('name', {}, 'val', {});
 matlabbatch{10}.spm.stats.fmri_spec.sess(1).multi_reg = {'/projects/dsnlab/shared/tag/nonbids_data/fMRI/fx/motion/wave1/auto-motion-fmriprep/rp_txt/with_manualEdits/rp_001_DSD1.txt'};
 matlabbatch{10}.spm.stats.fmri_spec.sess(1).hpf = 128;
 matlabbatch{10}.spm.stats.fmri_spec.sess(2).scans(1) = cfg_dep('Smooth: Smoothed Images', substruct('.','val', '{}',{9}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','files'));
 matlabbatch{10}.spm.stats.fmri_spec.sess(2).cond = struct('name', {}, 'onset', {}, 'duration', {}, 'tmod', {}, 'pmod', {}, 'orth', {});
-matlabbatch{10}.spm.stats.fmri_spec.sess(2).multi = {'/projects/dsnlab/shared/tag/nonbids_data/fMRI/fx/multiconds/dsd/wave1/NOD/001_DSD2_NOD.mat'};
+matlabbatch{10}.spm.stats.fmri_spec.sess(2).multi = {'/projects/dsnlab/shared/tag/nonbids_data/fMRI/fx/multiconds/dsd/wave1/NOD/pmod/001_DSD2_NOD.mat'};
 matlabbatch{10}.spm.stats.fmri_spec.sess(2).regress = struct('name', {}, 'val', {});
 matlabbatch{10}.spm.stats.fmri_spec.sess(2).multi_reg = {'/projects/dsnlab/shared/tag/nonbids_data/fMRI/fx/motion/wave1/auto-motion-fmriprep/rp_txt/with_manualEdits/rp_001_DSD2.txt'};
 matlabbatch{10}.spm.stats.fmri_spec.sess(2).hpf = 128;
