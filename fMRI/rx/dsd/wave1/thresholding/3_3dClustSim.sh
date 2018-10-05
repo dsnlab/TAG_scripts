582702e21ffe0b0fd435e350dae0a2c6afe89400
Nandi Vijayakumar <nanditav@talapas-ln1.cm.cluster>
2018-10-05 11:31:46 -0700

    bunch of random fmri scripts

--- fMRI/rx/dsd/wave1/thresholding/3_3dClustSim.sh
+++ fMRI/rx/dsd/wave1/thresholding/3_3dClustSim.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #--------------------------------------------------------------
 #
-#SBATCH --job-name=ind_ACF
-#SBATCH --output=output/ind_ACF_out.log
-#SBATCH --error=output/ind_ACF_err.log
+#SBATCH --job-name=3dClustSim
+#SBATCH --output=output/3dClustSim.log
+#SBATCH --error=output/3dClustSim_err.log
 #SBATCH --cpus-per-task=25
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=4000
@@ -16,7 +16,7 @@ module load afni
 rx_path=/projects/dsnlab/shared/tag/nonbids_data/fMRI/rx/dsd/wave1/pmod/MLmotion_FAST_RT/
 
 # Set path to the mask used in analyses 
-mask=/projects/dsnlab/shared/tag/nonbids_data/fMRI/fx/masks/wave1/groupMask_thresh10_bin.nii
+mask=/projects/dsnlab/shared/tag/nonbids_data/fMRI/fx/masks/wave1/dsd_groupMask_25perc.nii
 
 # Run 3dClustSim using the average acf outputs from the script "2_average_incACF.Rmd"
-3dClustSim -mask mask.nii -acf 0.6324164 4.917155 10.79975 > ${rx_path}/threshold.txt
+3dClustSim -mask $mask -acf 0.73527 4.6591 12.53 > ${rx_path}/threshold.txt
