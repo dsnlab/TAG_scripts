eca8c159b635c6680f569543259b4e1c2f3038cf
Dani Cosme <dani.cosme@gmail.com>
2018-11-06 15:50:13 -0800

    cleaned up scripts for project template testing

--- fMRI/fx/shell/schedule_spm_jobs/svc/wave1/event/archive/spm_job_residuals.sh
+++ fMRI/fx/shell/schedule_spm_jobs/svc/wave1/event/archive/spm_job_residuals.sh
@@ -1,22 +1,49 @@
 #!/bin/bash
 #--------------------------------------------------------------
-# This script:
+# Inputs:
+#	* SUB = defined in subject_list.txt
+#	* SCRIPT = MATLAB script to create and execute batch job
+#	* Edit SPM path
+#
+# Outputs:
 #	* Creates a batch job for $SUB
 #	* Batch jobs are saved to the path defined in MATLAB script
 #	* Executes batch job
-#	* Merges residuals for each run separately
-#	* Calculates ACF parameters for each run separately
-#	* Averages ACF parameters and saves in ACFparameters_average.txt
 #
-# D.Cos 2018.11.06
+# D.Cos 2017.3.7
 #--------------------------------------------------------------
 
-# set options and load matlab
+if [[ -z $1 || -z $2 || -z $3 ]]; then
+	if [[ -z $REPLACESID || -z $SCRIPT || -z $SUB ]]; then
+		echo "Aguments not supplied on command line or in environment"
+		exit 1
+	fi
+else
+	# define subject id to replace in script from input 1
+	REPLACESID=$1
+
+	# define matlab script to run from input 2
+	SCRIPT=$2
+
+	# define subject ID from input 3
+	SUB=$3
+fi
+
+# MATLAB version
+MATLABVER=R2015b
 SINGLECOREMATLAB=true
 ADDITIONALOPTIONS=""
 
 if "$SINGLECOREMATLAB"; then
 	ADDITIONALOPTIONS="-singleCompThread"
+fi
+
+
+if [ "${PROCESS}" == "slurm" ]; then
+	module load matlab
+	MATLABCOMMAND=matlab
+else
+	MATLABCOMMAND="/Applications/MATLAB_"${MATLABVER}".app/bin/matlab"
 fi
 
 # create and execute job
@@ -25,8 +52,7 @@ echo "${SUB}"
 echo "Running ${SCRIPT}"
 echo -------------------------------------------------------------------------------
 
-module load matlab
-matlab -nosplash -nodisplay -nodesktop ${ADDITIONALOPTIONS} -r "clear; addpath('$SPM_PATH'); spm_jobman('initcfg'); sub='$SUB'; script_file='$SCRIPT'; replacesid='$REPLACESID'; run('make_sid_matlabbatch.m'); spm_jobman('run',matlabbatch); exit"
+$MATLABCOMMAND -nosplash -nodisplay -nodesktop ${ADDITIONALOPTIONS} -r "clear; addpath('$SPM_PATH'); spm_jobman('initcfg'); sub='$SUB'; script_file='$SCRIPT'; replacesid='$REPLACESID'; run('make_sid_matlabbatch.m'); spm_jobman('run',matlabbatch); exit"
 
 # merge residual files
 echo -------------------------------------------------------------------------------
@@ -45,6 +71,7 @@ for i in $(seq 1 $RUNS)
 	fslmerge -t residuals_run${i} ${residual_files}
 	rm ${residual_files}
 done
+
 
 # run 3dFWHMx
 echo -------------------------------------------------------------------------------
