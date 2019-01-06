b747e9806d0610a170d3dd2b51bb26f9826ebe47
jessicaflannery <jflanner@uoregon.edu>
2019-01-06 10:39:46 -0800

    adding script to help with setup for xcp

---
+++ rsfMRI/xcpEngine_REST/setup_4_xcpReset.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+module load ants
+module load fsl
+module load afni
+module load c3d
+module load python3
+module load R
+
+export AFNI_PATH=/packages/afni/18.2.04/
+export XCPEDIR=/projects/dsnlab/shared/tag/TAG_scripts/rsfMRI/xcpEngine_REST
+export C3D_PATH=/projects/tau/packages/c3d/1.0.0/bin/c3d
+
+#now run xcpReset to make sure these were loaded properly
