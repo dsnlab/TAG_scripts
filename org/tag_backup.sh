0670822eab852063f166cfbcf361b50262a0abde
Zdena <zopdemac@uoregon.edu>
2019-11-25 14:57:43 -0800

    move laptops up, change simulator mac name

--- org/tag_backup.sh
+++ org/tag_backup.sh
@@ -2,6 +2,24 @@
 
 # This script copies (a) DICOMS from lcni to our lab, on Talapas, (b) DICOMS & physio from Talapas to CAS server
 
+## TASK BEH DATA (laptops first)
+#### Sherri to CAS
+rsync -aiv -e ssh FP@10.174.6.120:/Users/FP/Desktop/TAG-fMRI-task/task/input/tag*_info.mat /Volumes/psych-cog/dsnlab/TAG/behavior/task/info/ # inputs
+rsync -aiv -e ssh FP@10.174.6.120:/Users/FP/Desktop/TAG-fMRI-task/task/output/* /Volumes/psych-cog/dsnlab/TAG/behavior/task/output/ # outputs 10.109.94.238
+
+#### Terri to CAS - note that foldername is plural (taskS)
+rsync -aiv -e ssh FP@10.109.34.8:/Users/FP/Desktop/TAG-fMRI-tasks/task/input/tag*_info.mat /Volumes/psych-cog/dsnlab/TAG/behavior/task/info/ # inputs
+rsync -aiv -e ssh FP@10.109.34.8:/Users/FP/Desktop/TAG-fMRI-tasks/task/output/* /Volumes/psych-cog/dsnlab/TAG/behavior/task/output/ # outputs
+
+#### LCNI Desktop (backup) to CAS
+rsync -aiv -e ssh dsnlab@controlroommac.uoregon.edu:/Users/dsnlab/Desktop/TAG-fMRI-tasks/task/input/tag*_info.mat /Volumes/psych-cog/dsnlab/TAG/behavior/task/info/ # inputs
+rsync -aiv -e ssh dsnlab@controlroommac.uoregon.edu:/Users/dsnlab/Desktop/TAG-fMRI-tasks/task/output/* /Volumes/psych-cog/dsnlab/TAG/behavior/task/output/ # outputs
+
+#### Mock scanner to CAS
+rsync -aiv -e ssh dsnlab@simulatormac:/Users/dsnlab/Studies/TAG/task/DRS/task/input/tag*_info.mat /Volumes/psych-cog/dsnlab/TAG/behavior/task/info/ # inputs
+rsync -aiv -e ssh dsnlab@simulatormac:/Users/dsnlab/Studies/TAG/task/DRS/task/output/* /Volumes/psych-cog/dsnlab/TAG/behavior/task/output/ # outputs
+
+## NEURO + PHYSIO DATA
 ## COPY TALAPAS LCNI DICOMS TO TALAPAS DSNLAB ARCHIVE
 ssh jpfeifer@talapas-ln1.uoregon.edu rsync -aiv -P /projects/lcni/dcm/dsnlab/Pfeifer/TAG/TAG???_20* /projects/dsnlab/shared/tag/archive/DICOMS/ # for tag
 ssh jpfeifer@talapas-ln1.uoregon.edu rsync -aiv -P /projects/lcni/dcm/dsnlab/Pfeifer/SOS/TAG???_20* /projects/dsnlab/shared/sos/archive/DICOMS/ # for SOS (named correctly)
@@ -12,26 +30,10 @@ ssh jpfeifer@talapas-ln1.uoregon.edu rsync -aiv -P /projects/lcni/dcm/dsnlab/Pfe
 ##New TAG physio is now saved in dcm format… no additional backup required
 ##SOS physio is now saved in dcm format… no additional backup required
 
+# OTHER DATA SHUFFLING
 ## COPY TALAPAS DSNLAB ARCHIVE TO CAS
 rsync -aiv -e ssh jpfeifer@talapas-ln1.uoregon.edu:/projects/dsnlab/shared/tag/archive/ /Volumes/psych-cog/dsnlab/TAG/archive/ # tag dicoms and physio
 rsync -aiv -e ssh jpfeifer@talapas-ln1.uoregon.edu:/projects/dsnlab/shared/sos/archive/ /Volumes/psych-cog/dsnlab/SOS/archive/ # sos dicoms and physio
-
-## TASK BEH DATA 
-#### LCNI Desktop (backup) to CAS
-rsync -aiv -e ssh dsnlab@controlroommac.uoregon.edu:/Users/dsnlab/Desktop/TAG-fMRI-tasks/task/input/tag*_info.mat /Volumes/psych-cog/dsnlab/TAG/behavior/task/info/ # inputs
-rsync -aiv -e ssh dsnlab@controlroommac.uoregon.edu:/Users/dsnlab/Desktop/TAG-fMRI-tasks/task/output/* /Volumes/psych-cog/dsnlab/TAG/behavior/task/output/ # outputs
-
-#### Sherri to CAS
-rsync -aiv -e ssh FP@10.109.94.238:/Users/FP/Desktop/TAG-fMRI-task/task/input/tag*_info.mat /Volumes/psych-cog/dsnlab/TAG/behavior/task/info/ # inputs
-rsync -aiv -e ssh FP@10.109.94.238:/Users/FP/Desktop/TAG-fMRI-task/task/output/* /Volumes/psych-cog/dsnlab/TAG/behavior/task/output/ # outputs
-
-#### Terri to CAS - note that foldername is plural (taskS)
-rsync -aiv -e ssh FP@10.109.34.8:/Users/FP/Desktop/TAG-fMRI-tasks/task/input/tag*_info.mat /Volumes/psych-cog/dsnlab/TAG/behavior/task/info/ # inputs
-rsync -aiv -e ssh FP@10.109.34.8:/Users/FP/Desktop/TAG-fMRI-tasks/task/output/* /Volumes/psych-cog/dsnlab/TAG/behavior/task/output/ # outputs
-
-#### Mock scanner to CAS
-rsync -aiv -e ssh dsnlab@simulatum:/Users/dsnlab/Studies/TAG/task/DRS/task/input/tag*_info.mat /Volumes/psych-cog/dsnlab/TAG/behavior/task/info/ # inputs
-rsync -aiv -e ssh dsnlab@simulatum:/Users/dsnlab/Studies/TAG/task/DRS/task/output/* /Volumes/psych-cog/dsnlab/TAG/behavior/task/output/ # outputs
 
 ## COPY ALL FILES FROM TAG TO EXTERNAL HARDDRIVE ("Chief")
 rsync -aiv -P /Volumes/psych-cog/dsnlab/TAG/ /Volumes/Chief/TAG/ # tag
