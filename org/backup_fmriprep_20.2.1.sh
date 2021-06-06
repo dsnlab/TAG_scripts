4d65b9a588e53390ff80d9ee37fd905c69f96137
Theresa Cheng <tcheng@uoregon.edu>
2021-06-05 23:19:14 -0700

    backup script for current analyses

---
+++ org/backup_fmriprep_20.2.1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script backs up the fmriprep and xcp output folders from Talapas to CAS
+# T Cheng | 6/5/2021
+
+# Create log file with today's date:
+log_name=$(date +%Y-%m-%d-%H-%M-%S)_rsync_fmriprep_20.2.1.log
+touch $log_name
+
+## COPY TALAPAS DSNLAB ARCHIVE TO CAS
+rsync -aiv --log-file=$log_name -e ssh jpfeifer@talapas-ln1.uoregon.edu:/projects/dsnlab/shared/tag/fmriprep_20.2.1/fmriprep/* /Volumes/psych-cog/dsnlab/TAG/bids_data/derivatives/fmriprep_20.2.1/fmriprep # fmriprep processed output files from bids_data_sep_wave
+
+# rsync -aiv -e ssh jpfeifer@talapas-ln1.uoregon.edu:/projects/dsnlab/shared/tag/fmriprep_20.2.1/xcp_output/* /Volumes/psych-cog/dsnlab/TAG/bids_data/derivatives/fmriprep_20.2.1/xcp_output # outputs from xcp engine 
