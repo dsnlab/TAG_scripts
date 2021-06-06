22bad06a4ccedbd2a0bc4912ede9641836965055
Theresa Cheng <tcheng@uoregon.edu>
2021-06-05 23:23:50 -0700

    exclude html files

--- org/backup_fmriprep_20.2.1.sh
+++ org/backup_fmriprep_20.2.1.sh
@@ -8,6 +8,6 @@ log_name=$(date +%Y-%m-%d-%H-%M-%S)_rsync_fmriprep_20.2.1.log
 touch $log_name
 
 ## COPY TALAPAS DSNLAB ARCHIVE TO CAS
-rsync -aiv --log-file=$log_name -e ssh jpfeifer@talapas-ln1.uoregon.edu:/projects/dsnlab/shared/tag/fmriprep_20.2.1/fmriprep/* /Volumes/psych-cog/dsnlab/TAG/bids_data/derivatives/fmriprep_20.2.1/fmriprep # fmriprep processed output files from bids_data_sep_wave
+rsync -aiv --log-file=$log_name --exclude".html" -e ssh jpfeifer@talapas-ln1.uoregon.edu:/projects/dsnlab/shared/tag/fmriprep_20.2.1/fmriprep/* /Volumes/psych-cog/dsnlab/TAG/bids_data/derivatives/fmriprep_20.2.1/fmriprep # fmriprep processed output files from bids_data_sep_wave
 
 # rsync -aiv -e ssh jpfeifer@talapas-ln1.uoregon.edu:/projects/dsnlab/shared/tag/fmriprep_20.2.1/xcp_output/* /Volumes/psych-cog/dsnlab/TAG/bids_data/derivatives/fmriprep_20.2.1/xcp_output # outputs from xcp engine 
